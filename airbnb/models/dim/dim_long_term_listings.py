d8e5185f16db0ce61f3cfdb4a20537cf0d5c4e33
Filipe Romano Souto de Castro <fcastro@caij.qc.ca>
2026-05-15 22:47:52 -0400

    Section 24 started dbt power user

--- airbnb/models/dim/dim_long_term_listings.py
+++ airbnb/models/dim/dim_long_term_listings.py
@@ -2,4 +2,4 @@ def model(dbt, session):
     listings = dbt.ref("dim_listings_cleansed")
 
     return (listings.filter(listings["MINIMUM_NIGHTS"] >= 30)
-            .select("LISTINN_ID", "LISTING_NAME", "PRICE"))
+            .select("LISTING_ID", "LISTING_NAME", "PRICE"))
